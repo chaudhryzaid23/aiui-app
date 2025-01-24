647dba8e39acfd2de7a141ba62c7e5929c837fa1
zaid hassan <chaudhryzaid23@gmail.com>
2025-01-24 10:54:06 +0500

    layout component working independently

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,17 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
-  template: `
-    <mat-toolbar color="primary">AI Query Interface</mat-toolbar>
-    <div class="layout-container">
-      <app-ai-query
-        (paragraphSelected)="onParagraphSelected($event)"
-      ></app-ai-query>
-      <app-markdown-editor
-        [markdownText]="markdownContent"
-      ></app-markdown-editor>
-    </div>
-  `,
+  templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent {
